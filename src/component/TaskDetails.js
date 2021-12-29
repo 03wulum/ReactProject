19c808f3ea1605bf55887be17b385142257ff186
Obinna Ewulum] <obinnaewulum@gmail.com>
2021-12-28 20:41:52 -0500

    Router v6 changes implemented from tutorial. Discovered error in json server endpoint url.

--- src/component/TaskDetails.js
+++ src/component/TaskDetails.js
@@ -1,46 +1,33 @@
 import { useState, useEffect } from "react";
-import {
-  useParams,
-  Navigate,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import Button from "./Button";
 function TaskDetails() {
   const [loading, setLoading] = useState(true);
   const [task, setTask] = useState({});
-  const [error, setError] = useState(null);
 
   const params = useParams();
   const navigate = useNavigate();
   // destructure by getting pathname out of useLocation
-  const { pathname } = useLocation();
-
-  console.log("check params", params);
+  //   const { pathname } = useLocation();
 
   useEffect(() => {
     const fetchTask = async () => {
-      const res = await fetch(`http://localhost:5000/task/${params.id}`);
+      const res = await fetch(`http://localhost:5000/tasks/${params.id}`);
       const data = await res.json();
-
-      if (res.status === 404) {
-        //   setError('task Not found')
-        navigate("/");
-      }
 
       setTask(data);
       setLoading(false);
+      if (res.status === 404) {
+        navigate("/");
+      }
     };
 
     fetchTask();
-  });
-
-  //   if (error)
+  }, []);
   return loading ? (
     <h3>Loading...</h3>
   ) : (
     <div>
-      <p>{pathname}</p>
       <h3>{task.text}</h3>
       <p>{task.day}</p>
       <Button
